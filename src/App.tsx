5996e159bba8ef5b2893f62560897718004dedae
nstrohmayer <Strohmayer.Noah@hotmail.com>
2023-04-13 14:48:55 +0200

    dbd maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './App.css';
 import Counter from './views/counter/Counter';
 import ErrorPage from './error-page';
 import Home from './views/home/Home';
 import Pokemon from './views/pokemon/Pokemon';
 import PokemonWithStore from './views/pokemon/Pokemon_withStore';
+import MapsComponent from "./views/deadByDaylight/MapsComponent";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />,
-        errorElement: <ErrorPage />,
+        element: <Home/>,
+        errorElement: <ErrorPage/>,
     }, {
         path: '/counter',
-        element: <Counter />,
+        element: <Counter/>,
     }, {
         path: '/pokemon',
-        element: <Pokemon />,
+        element: <Pokemon/>,
     }, {
         path: '/pokemonWithStore',
-        element: <PokemonWithStore />,
-    },
+        element: <PokemonWithStore/>,
+    }, {
+        path: '/dead-by-daylight/maps',
+        element: <MapsComponent/>
+    }
 ]);
 
 function App() {
     return (
         <div className="App">
-            <RouterProvider router={ router } />
+            <RouterProvider router={router}/>
         </div>
     );
 }
