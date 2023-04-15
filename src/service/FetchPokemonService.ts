5b77006fd7bec59654bdd36b98a1c11d8d8457e4
nstrohmayer <Strohmayer.Noah@hotmail.com>
2023-04-15 21:54:19 +0200

    revert attempt to use useQuery to fetch all pokemon

--- src/service/FetchPokemonService.ts
+++ src/service/FetchPokemonService.ts
@@ -1,11 +1,16 @@
 import {isPokemonDTO, PokemonDTO} from './PokemonDTO';
-import pokemonApi from "./PokemonApi";
+import axios, {AxiosInstance} from "axios/index";
 
 class FetchPokemonService {
 
+    private axiosInstance: AxiosInstance = axios.create({
+        baseURL: 'https://pokeapi.co/api/v2/',
+        timeout: 1000,
+    })
+
     async fetchPokemon(id: number): Promise<PokemonDTO> {
         const url = `pokemon/${id}`;
-        const responseData: unknown = (await pokemonApi.axiosInstance.get(url)).data;
+        const responseData: unknown = (await this.axiosInstance.get(url)).data;
 
         if (isPokemonDTO(responseData)) {
             return responseData
--- src/views/pokemon/Pokemon_withStore.tsx
+++ src/views/pokemon/Pokemon_withStore.tsx
@@ -1,14 +1,10 @@
 import React, {ChangeEvent} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Dispatch, RootState} from '../../store';
-import {PokemonDTO} from "../../service/PokemonDTO";
-import {useFetchAllPokemon} from "../../service/UseFetchAllPokemon";
 
 const PokemonWithStore: React.FC = () => {
   const selectedPokemon = useSelector((state: RootState) => state.pokemon.selectedPokemon);
   const dispatch = useDispatch<Dispatch>();
-
-  const {status, data, isFetching} = useFetchAllPokemon()
 
   async function handleInputChanged(event: ChangeEvent<HTMLInputElement>) {
     // TODO debouncing
@@ -22,30 +18,12 @@ const PokemonWithStore: React.FC = () => {
     await dispatch.pokemon.selectPokemon(id);
   }
 
-  console.log(data);
-  console.log(data !== undefined)
-  data !== undefined && data?.map((item) => {
-    console.log(item.name)
-  })
-
   return (
       <div>
         <div>
           <h1>Pokemon Lookup</h1>
           <input onChange={handleInputChanged}/>
           <p>{selectedPokemon?.name}</p>
-        </div>
-        <div>
-          <h1>Pokemon List</h1>
-          <p> {isFetching ? "loading" : undefined}</p>
-          TODO list all pokemon
-          {typeof data}
-          <p>{status}</p>
-          {
-              data !== undefined && data?.map((pokemon, index) => {
-              return <p key={index}>{pokemon.name}</p>
-            })
-          }
         </div>
         <a href="/">Back</a>
       </div>
